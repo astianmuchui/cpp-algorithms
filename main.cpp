331d5a4c175a1c487795b0b481970db4c162c0f5
astian4 <sebastianmuchui79@gmail.com>
2021-04-09 20:32:31 +0300

    Learned a bit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // std::cout << "Hello world\n";
+    // std::cout << "Welcome to c++\n";
+
+
+
+    //Data types
+
+    const int age = 25;
+
+
+    //floats
+
+    float weight = 650.5;
+
+    double balance = 1245.67;
+
+
+    char gender = "male";
+
+    string name = "Seb Astian";
+
+    bool iamcool = true;
+
+
+    string colors[] = {"red","green","blue"};
+
+    cout<<colors[0];
+	cin>> colours[0];
+	colors[1] = "grey";
+
+
+
+
+	bool itsrainy = false;
+	if(itsrainy){
+	cout<<"Take an umbrella";	
+}else{
+	cout<<"Go bask";
+}	
+
+}
+
+
+
+
